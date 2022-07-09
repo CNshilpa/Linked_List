4683aeaa6fe4f284b5132e46069f8a72f766c937
CNshilpa <shilpa.cnv@gmail.com>
2022-07-09 13:41:45 +0530

    Addition : Linked List Operations

---
+++ src/LinkeListMain.java
@@ -0,0 +1,22 @@
+
+public class LinkeListMain {
+	public static void main(String[] args) {
+
+		LinkedList linkedlist = new LinkedList();
+
+		System.out.println("LINKED LIST");
+
+		linkedlist.addLast(56);
+		linkedlist.display();
+		linkedlist.addLast(70);
+		linkedlist.display();
+		linkedlist.insertAtSpecificIndex(1, 30);
+		linkedlist.display();
+		System.out.println("Inserting Element");
+		linkedlist.insertAtSpecificIndex(2, 40);
+		linkedlist.display();
+		linkedlist.deleteAtSpecificIndex(2);
+		System.out.println("The Size of the LinkedList is :" + linkedlist.size());
+		linkedlist.display();
+	}
+}
